dc5f40abc565ae82894f37fcab46715c52b8322b
Nejc <nejcrogelsek0@gmail.com>
2022-05-29 16:22:57 +0200

    cache data

--- src/pages/RQSuperHeroes/RQSuperHeroes.tsx
+++ src/pages/RQSuperHeroes/RQSuperHeroes.tsx
@@ -1,12 +1,38 @@
 import { FC } from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { SuperHero } from 'interfaces'
 
 interface Props {}
 
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
 const RQSuperHeroes: FC<Props> = (props: Props) => {
+  const { data, isLoading, isError, error, isFetching } = useQuery('super-heroes', fetchSuperHeroes, {
+    cacheTime: 3000, // default time is set to 5 minutes
+  })
+
+  console.log({ isLoading, isFetching })
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (isError && error instanceof Error) {
+    return <h2>{error.message}</h2>
+  }
+
   return (
-    <div className="rq-super-heroes">
-      <h1>React Query SuperHeroes</h1>
-    </div>
+    <>
+      <h2>RQ Super Heroes</h2>
+      <ul>
+        {data?.data.map((hero: SuperHero, index: number) => (
+          <li key={index}>{hero.name}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
