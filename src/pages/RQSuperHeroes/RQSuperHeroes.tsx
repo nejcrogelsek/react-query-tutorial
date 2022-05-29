b156c849b30580aa886a8964cef7479ec339c2c2
Nejc Rogelšek <nejcrogelsek0@gmail.com>
2022-05-29 19:21:04 +0200

    Update RQSuperHeroes.tsx

--- src/pages/RQSuperHeroes/RQSuperHeroes.tsx
+++ src/pages/RQSuperHeroes/RQSuperHeroes.tsx
@@ -12,12 +12,10 @@ const fetchSuperHeroes = () => {
 const RQSuperHeroes: FC<Props> = (props: Props) => {
   const onError = (error: Error) => {
     console.log('Perform side effect after encountering error', error)
-    setInterval(false)
   }
 
   const onSuccess = (response: any) => {
     console.log('Perform side effect after encountering error', response)
-    if (response.data.length >= 4) setInterval(false)
   }
 
   const { data, isLoading, isError, error, isFetching } = useQuery('super-heroes', fetchSuperHeroes, {
