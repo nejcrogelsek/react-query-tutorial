2dd30422a3479b68cd3d15a16105389ca371ab5f
Nejc <nejcrogelsek0@gmail.com>
2022-05-29 15:52:02 +0200

    Setup react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 //Partials
 import Header from './components/partials/Header/Header'
 import Routes from './routes/Routes'
 
+const queryClient = new QueryClient()
+
 const App: FC = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Routes />
-    </>
+    </QueryClientProvider>
   )
 }
 
