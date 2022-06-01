e8aabf9633a66ccfc17e47fbcc37c1fd786b5e0c
Nejc <nejcrogelsek0@gmail.com>
2022-06-01 20:17:02 +0200

    Basic post request

--- src/lib/hooks/useSuperHeroesData.tsx
+++ src/lib/hooks/useSuperHeroesData.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation } from 'react-query'
 import axios from 'axios'
 import { SuperHero } from 'interfaces'
 
@@ -11,9 +11,17 @@ const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
 
+const addSuperHero = (hero: { name: string; alterEgo: string }) => {
+  return axios.post('http://localhost:4000/superheroes', hero)
+}
+
 export const useSuperHeroesData = ({ onSuccess, onError }: Props) => {
   return useQuery('super-heroes', fetchSuperHeroes, {
     onError,
     onSuccess,
   })
+}
+
+export const useAddSuperHeroData = () => {
+  return useMutation(addSuperHero)
 }
