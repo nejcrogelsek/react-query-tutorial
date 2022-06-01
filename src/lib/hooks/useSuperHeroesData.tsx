969b987a6dda5d9bdbc69a4c37c3b690b1ead9a3
Nejc <nejcrogelsek0@gmail.com>
2022-06-01 20:19:22 +0200

    Query invalidation

--- src/lib/hooks/useSuperHeroesData.tsx
+++ src/lib/hooks/useSuperHeroesData.tsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 import { SuperHero } from 'interfaces'
 
@@ -23,5 +23,10 @@ export const useSuperHeroesData = ({ onSuccess, onError }: Props) => {
 }
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero)
+  const queryClient = useQueryClient()
+  return useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('super-heroes')
+    },
+  })
 }
